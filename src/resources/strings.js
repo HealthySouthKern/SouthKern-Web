84e555dbc654a8a2c3856a50e37085b37325f8ef
JFschmiechen <jfschmiechen@gmail.com>
2019-04-20 14:35:50 -0700

    Updated strings to show what resources are required

--- src/resources/strings.js
+++ src/resources/strings.js
@@ -1,4 +1,8 @@
 
 const firebase_api_key = '';
 
-export default { firebase_api_key }
+const sendbird_api_key = '';
+
+const sendbird_app_id = '';
+
+export default { firebase_api_key, sendbird_api_key, sendbird_app_id }
